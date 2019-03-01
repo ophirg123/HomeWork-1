ef923ae0233301ae124b36ee14e153e775c0c0c3
ophirg123 <gruteke@post.bgu.ac.il>
2019-03-01 15:06:36 +0200

    Prime

--- 302363049.py
+++ 302363049.py
@@ -11,16 +11,18 @@ if args.task in TaskList and args.N in range(1, 1001):
     if args.task == 'pi':
         print('pi')
     elif args.task == 'prime':
-        PrimeList = []
-        num = 0
-        for num in range(1,2000):
-            while len(PrimeList) < 1000:
-                for i in range(1, 2000):
-                    if num % i == 0:
-                        PrimeList.append(num)
-                print(PrimeList)
-        else:
-            print(args.N)
+        List_length = 0
+        prime_List = [1]
+        optional_List = list(range(10000))
+        for each_number in range(2, 10000):
+            if optional_List[each_number]:
+                prime_List.append(each_number)
+                for every_multiple_of_the_prime in range(each_number * 2, 10000, each_number):
+                    optional_List[every_multiple_of_the_prime] = 0
+            List_length = len(prime_List)
+            if List_length == 1001:
+                break
+        print(prime_List[args.N])
     else:
         f0 = 0
         f1 = 1
--- Prime.py
+++ Prime.py
@@ -1,9 +1,13 @@
-PrimeList = []
-num = 0
-while len(PrimeList) < 1000:
-    for num in range(1,2000):
-        for i in range(2, 2000):
-            if num % i == 0:
-                PrimeList.append(num)
-            break
-        print(PrimeList)
+List_length = 0
+prime_List = [1]
+optional_List = list(range(10000))
+for each_number in range(2, 10000):
+    if optional_List[each_number]:
+        prime_List.append(each_number)
+        for every_multiple_of_the_prime in range(each_number * 2, 10000, each_number):
+            optional_List[every_multiple_of_the_prime] = 0
+    List_length = len(prime_List)
+    if List_length == 1000:
+        break
+print(prime_List)
+print(List_length)
